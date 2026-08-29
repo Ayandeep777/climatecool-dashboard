b2a00919b56beed162ceb73c0b668eba6c9ff75e
Ayandeep777 <118059299+Ayandeep777@users.noreply.github.com>
2026-08-29 21:25:14 +0530

    Create demand_engine.py

---
+++ pages/demand_engine.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render(app_data):
+    st.title("📈 Demand Engine")
+    st.write("Demand forecasting and analysis")
+    st.info("This page is under development. Coming soon!")
