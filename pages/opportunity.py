ebeac2220e845d7a373effe78a43bb1cf0ee2ea8
Ayandeep777 <118059299+Ayandeep777@users.noreply.github.com>
2026-08-29 21:24:27 +0530

    Add opportunity page with initial content
    Initial implementation of the opportunity page with a title and placeholder text.

---
+++ pages/opportunity.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render(app_data):
+    st.title("📍 Opportunity")
+    st.write("District Ranking and Opportunity Analysis")
+    st.info("This page is under development. Coming soon!")
