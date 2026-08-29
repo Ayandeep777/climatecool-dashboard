e21b599fa94b84073156dbd27da2ff9854cca98f
Ayandeep777 <118059299+Ayandeep777@users.noreply.github.com>
2026-08-29 21:26:30 +0530

    Add financials page with basic layout and info

---
+++ pages/financials.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render(app_data):
+    st.title("💰 Financials")
+    st.write("Financial performance and metrics")
+    st.info("This page is under development. Coming soon!")
