9ae1fc4a34ebd22a84c9de2ace1ef20f5355ab6f
Ayandeep777 <118059299+Ayandeep777@users.noreply.github.com>
2026-08-29 21:26:11 +0530

    Add heat_trigger_marketing.py for marketing campaigns

---
+++ pages/heat_trigger_marketing.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render(app_data):
+    st.title("📢 Heat-Trigger Marketing")
+    st.write("Marketing performance and heat-triggered campaigns")
+    st.info("This page is under development. Coming soon!")
