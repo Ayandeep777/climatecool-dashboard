39a1ee6aecdcd2fa0bb8a96ba7bf140bf1be583d
Ayandeep777 <118059299+Ayandeep777@users.noreply.github.com>
2026-08-29 21:25:54 +0530

    Add SKU and Dealer analysis page

---
+++ pages/sku_dealer.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def render(app_data):
+    st.title("🛒 SKU & Dealer")
+    st.write("SKU performance and dealer analysis")
+    st.info("This page is under development. Coming soon!")
